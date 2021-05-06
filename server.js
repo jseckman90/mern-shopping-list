07cec1deb88e193ea9063b21ee9215ea493bad8f
Josh Seckman <jseckman90@gmail.com>
2021-05-06 16:44:22 -0400

    item route working

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const items = require("./routes/api/items");
 
 const app = express();
 
 //Body parser middleware
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
 
 //db config
 const db = require("./config/keys").mongoURI;
@@ -16,6 +20,14 @@ mongoose
     console.log("mongoDB Connected");
   })
   .catch((err) => console.log(err));
+
+// Use Routes
+app.use("/api/items", items);
+
+app.get("/", (req, res) => {
+  res.send("hello from 5000");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
